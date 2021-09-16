c5a2580dbb0b81e6bb1e71bbc82256b89c76a1ee
Pratik <pratikrajaryan@gmail.com>
2021-09-16 12:46:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'
+import questions from './Questions'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function App() {
+	const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [showScore, setShowScore] = useState(false);
+	const [score, setScore] = useState(0);
+
+	const handleAnswerOptionClick = (isCorrect) => {
+		if (isCorrect) {
+			setScore(score + 1);
+		}
+
+		const nextQuestion = currentQuestion + 1;
+		if (nextQuestion < questions.length) {
+			setCurrentQuestion(nextQuestion);
+		} else {
+			setShowScore(true);
+		}
+	};
+	return (
+        <div id="bg">
+        <nav>
+        <h3>Web Development Quiz</h3>
+        <button>
+        Logout
+        </button>
+        </nav>
+		<div className='scoreboard'>
+		       <h2>	Score {score}/{questions.length}</h2>				
+				</div>
+        <div className='app'>
+			{showScore ? (
+				<div className='score-section'>
+					You scored {score} out of {questions.length}
+				</div>
+			) : (
+				<>
+					<div className='question-section'>
+						<div className='question-count'>
+							<span>Question {currentQuestion + 1}</span>/{questions.length}
+						</div>
+						<div className='question-text'>{questions[currentQuestion].questionText}</div>
+					</div>
+					<div className='answer-section'>
+						{questions[currentQuestion].answerOptions.map((answerOption) => (
+							<button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+						))}
+					</div>
+				</>
+			)}
+		</div>
+        </div>
+	);
 }
-
-export default App;
---
+++ src/Questions.js
@@ -0,0 +1,39 @@
+export default [
+    {
+      questionText: 'CSS stands for?',
+      answerOptions: [
+        { answerText: 'Cascade style sheets', isCorrect: false },
+        { answerText: 'Color and style sheets', isCorrect: false },
+        { answerText: 'Cascading style sheets', isCorrect: true },
+        { answerText: 'None of the above', isCorrect: false },
+      ],
+    },
+    {
+      questionText: 'The HTML attribute used to define the inline styles is -',
+      answerOptions: [
+        { answerText: 'styles', isCorrect: false },
+        { answerText: 'style', isCorrect: true },
+        { answerText: 'class', isCorrect: false },
+        { answerText: 'None of the above', isCorrect: false },
+      ],
+    },
+    {
+      questionText: 'The CSS property used to specify the transparency of an element is ?',
+      answerOptions: [
+        { answerText: 'Opacity', isCorrect: true },
+        { answerText: 'filter', isCorrect: false },
+        { answerText: 'Visibility', isCorrect: false },
+        { answerText: 'Overlay', isCorrect: false },
+      ],
+    },
+    {
+      questionText: 'How to select the elements with the class name "example"?',
+      answerOptions: [
+        { answerText: '#example', isCorrect: false },
+        { answerText: 'class example', isCorrect: false },
+        { answerText: 'example', isCorrect: false },
+        { answerText: '.example', isCorrect: true },
+      ],
+    },
+    ];
+    
